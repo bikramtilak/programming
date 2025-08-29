dc6a5eeb066e18c21adc7a025a51c1fd616ad8ba
bikramtilak <tilak.bikram2004@gmail.com>
2025-08-29 09:37:21 +0530

    Add files via upload

---
+++ swap numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+int a,b,temp;
+printf("Enter value of a");
+scanf("%d",&a);
+printf("Enter the value of b");
+scanf("%d",&b);
+temp=a;
+a=b;
+b=temp;
+printf("After swapping:\n");
+printf("a=%d\n",a);
+printf("b=%d\n",b);
+return 0;
+}
