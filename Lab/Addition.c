8476644472fec03e7b748845038dd856c2616a1f
bikramtilak <tilak.bikram2004@gmail.com>
2025-09-29 22:36:06 +0530

    Update Addition.c

--- Lab/Addition.c
+++ Lab/Addition.c
@@ -1,11 +1,1 @@
-#include<stdio.h>
-int main(){
-  int a,b,c;
-printf("the value of a=\n");
-scanf("%d",&a);
-printf("the value of b=\n");
-scanf("%d",&b);
-c=a+b;
-printf("sum c=%d",c);
-return 0;
-}
+
